929b71e35827244c6f03b06e5b45159635318dc9
Tch1b0 <Jpour3006@gmail.com>
2023-03-23 15:06:43 +0100

    optimize workflow

--- bincomp.py
+++ bincomp.py
@@ -1,6 +1,15 @@
 import sys
 
+verbose_mode = any(x == "-v" or x == "--verbose" for x in sys.argv)
+
+
+def verbose_print(*string):
+    if verbose_mode:
+        print(*string)
+
+
 a, b = [], []
+equal_counter = 0
 
 with open(sys.argv[1], "rb") as f:
     a = f.read()
@@ -8,7 +17,18 @@ with open(sys.argv[1], "rb") as f:
 with open(sys.argv[2], "rb") as f:
     b = f.read()
 
+a_len = len(a)
+b_len = len(b)
+
+if a_len != b_len:
+    print(f"Different file lengths! {a_len} != {b_len}")
+
 for i, v in enumerate(zip(a, b)):
     va, vb = v
     if va != vb:
-        print(f"Difference in Byte {i + 1} detected: {bin(va)} != {bin(vb)}")
+        verbose_print(
+            f"Difference in Byte {i + 1} detected: {bin(va)} != {bin(vb)}")
+    else:
+        equal_counter += 1
+
+print(f"File equality {equal_counter / a_len * 100:.2f}%")
